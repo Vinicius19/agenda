7ccb6bb9dd16a106f77bc4d3216b399fb31de029
vinicius <viniciuspassos05@gmail.com>
2023-06-25 19:52:23 -0300

    projeto agenda

--- projeto/agenda.py
+++ projeto/agenda.py
@@ -1,128 +1,87 @@
 import sys
 import psycopg2
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QDialog, QLabel, QVBoxLayout, QPushButton, QMenu, QAction, QLineEdit, QListWidget, QInputDialog
+import calendar
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QPushButton, QLineEdit
 
-
-class EventDialog(QDialog):
-    def __init__(self, date, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle("Inserir Evento")
-        
-        layout = QVBoxLayout(self)
-        
-        label = QLabel(f"Data: {date}")
-        layout.addWidget(label)
-        
-        self.event_list = QListWidget()
-        layout.addWidget(self.event_list)
-        
-        self.event_input = QLineEdit()
-        layout.addWidget(self.event_input)
-        
-        button_layout = QHBoxLayout()
-        
-        add_button = QPushButton("Adicionar")
-        add_button.clicked.connect(self.add_event)
-        button_layout.addWidget(add_button)
-        
-        remove_button = QPushButton("Remover")
-        remove_button.clicked.connect(self.remove_event)
-        button_layout.addWidget(remove_button)
-        
-        layout.addLayout(button_layout)
-        
-        self.date = date
-        self.load_events()
-    
-    def load_events(self):
-        self.event_list.clear()
-        
-        try:
-            conn = psycopg2.connect(
-                host="localhost",
-                port="5432",
-                database="Projeto",
-                user="postgres",
-                password="89iokl,."
-            )
-            
-            cursor = conn.cursor()
-            cursor.execute("SELECT contatos FROM public.contatos WHERE data = %s", (self.date,))
-            
-            events = cursor.fetchall()
-            
-            for event in events:
-                self.event_list.addItem(event[0])
-            
-            cursor.close()
-            conn.close()
-        
-        except psycopg2.Error as e:
-            print("Erro ao carregar contatos:", e)
-    
-    def add_event(self):
-        event = self.event_input.text()
-        if event:
-            try:
-                conn = psycopg2.connect(
-                    host="localhost",
-                    port="5432",
-                    database="Projeto",
-                    user="postgres",
-                    password="89iokl,."
-                )
-                
-                cursor = conn.cursor()
-                cursor.execute("INSERT INTO public.contatos (codigo, nome, telefone, data, evento) VALUES (%s, %s, %s, %s, %s)", (self.codigo, nome, telefone, data, evento))
-                conn.commit()
-                cursor.close()
-                conn.close()
-                
-                self.event_list.addItem(event)
-                self.event_input.clear()
-            
-            except psycopg2.Error as e:
-                print("Erro ao adicionar evento:", e)
-    
-    def remove_event(self):
-        selected_items = self.event_list.selectedItems()
-        
-        if not selected_items:
-            return
-        
-        event = selected_items[0].text()
-        
-        try:
-            conn = psycopg2.connect(
-                host="localhost",
-                port="5432",
-                database="Projeto",
-                user="postgres",
-                password="89iokl,."
-            )
-            
-            cursor = conn.cursor()
-            cursor.execute("DELETE FROM public.contatos WHERE data = %s AND evento = %s", (self.data, evento))
-            conn.commit()
-            cursor.close()
-            conn.close()
-            
-            self.event_list.takeItem(self.event_list.row(selected_items[0]))
-        
-        except psycopg2.Error as e:
-            print("Erro ao remover evento:", e)
-
-
-class CalendarApp(QMainWindow):
+class AgendaApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        
-        self.setWindowTitle("Calendário")
-        self.setGeometry(100, 100, 600, 500)
-        
+
+        self.setWindowTitle("Agenda")
+        self.setGeometry(100, 100, 500, 400)
+
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout(self.central_widget)
+
+        self.table_widget = QTableWidget()
+        self.table_widget.setColumnCount(5)
+        self.table_widget.setHorizontalHeaderLabels(['Código', 'Nome', 'Telefone', 'Data', 'Evento'])
         
-        layout = QVBoxLayout(self.central_widget)
         
-        self.table_widget = Q
+        
+        self.layout.addWidget(self.table_widget)
+
+        self.input_codigo   = QLineEdit()
+        self.input_nome     = QLineEdit()
+        self.input_telefone = QLineEdit()
+        self.input_data     = QLineEdit()
+        self.input_evento   = QLineEdit()
+        self.btn_add = QPushButton("Adicionar Evento")
+
+        self.layout.addWidget(self.input_codigo)
+        self.layout.addWidget(self.input_nome)
+        self.layout.addWidget(self.input_telefone)
+        self.layout.addWidget(self.input_data)
+        self.layout.addWidget(self.input_evento)
+        self.layout.addWidget(self.btn_add)
+
+        self.btn_add.clicked.connect(self.add_event)
+        
+        self.db_connection = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            database="Projeto",
+            user="postgres",
+            password="89iokl,."
+        )
+
+    def add_event(self):
+        
+        codigo   = self.input_codigo.text()
+        nome     = self.input_nome.text()
+        telefone = self.input_telefone.text()
+        data     = self.input_data.text()
+        evento   = self.input_evento.text()
+        
+        
+        row_count = self.table_widget.rowCount()
+        self.table_widget.insertRow(row_count)
+
+        cursor = self.db_connection.cursor()
+        cursor.execute("INSERT INTO public.contatos(codigo, nome, telefone, data, evento)VALUES (%s, %s, %s,%s,%s)", (codigo, nome, telefone, data, evento))
+        self.db_connection.commit()
+        cursor.close()
+
+        row_count = self.table_widget.rowCount()
+        self.table_widget.insertRow(row_count)
+        
+        self.table_widget.setItem(row_count, 1, QTableWidgetItem(codigo))
+        self.table_widget.setItem(row_count, 2, QTableWidgetItem(nome))
+        self.table_widget.setItem(row_count, 3, QTableWidgetItem(telefone))
+        self.table_widget.setItem(row_count, 4, QTableWidgetItem(data))
+        self.table_widget.setItem(row_count, 5, QTableWidgetItem(evento))
+ 
+        self.input_codigo.clear()
+        self.input_nome.clear()
+        self.input_telefone.clear()
+        self.input_data.clear()
+        self.input_evento.clear()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    agenda = AgendaApp()
+    agenda.show()
+    sys.exit(app.exec_())
